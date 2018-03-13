023a632464b77027a1fcf5c93ee766fecb6e7fd5
jongtelles <jongtelles@gmail.com>
2018-03-12 22:56:13 -0400

    Full functionality

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,12 +1,32 @@
 let peopleArr = [{
-        name: 'Shane Chan',
-        photo: 'url',
-        scores: [1, 2, 3, 4, 5, 1, 2, 3, 4, 5]
+        name: 'Shinsuke Nakamura',
+        photo: 'https://www.wwe.com/f/styles/og_image/public/all/2017/06/Shinsuke_Nakamura_bio--052c8c487f9fda469802509d4ca7e44e.jpg',
+        scores: [4, 5, 2, 3, 1, 1, 2, 4, 1, 2]
     },
     {
-        name: 'Memo T Shane',
-        photo: 'url',
-        scores: [1, 2, 3, 4, 5, 1, 1, 1, 2, 3]
+        name: 'AJ Styles',
+        photo: 'http://www.pwmania.com/wp-content/uploads/2017/07/aj-styles-1-696x464.jpg',
+        scores: [5, 4, 5, 2, 5, 2, 5, 1, 3, 1]
+    },
+    {
+        name: 'Ember Moon',
+        photo: 'http://www.wwe.com/f/2017/12/sneak_musicp10_108--4eb962136421e1d678c238de91d9f552.jpg',
+        scores: [5, 5, 4, 2, 4, 5, 2, 2, 5, 2]
+    },
+    {
+        name: 'Adam Cole (Baybay!)',
+        photo: 'https://cdn3.whatculture.com/images/2018/01/971134ef9bc861aa-600x400.jpg',
+        scores: [2, 2, 3, 2, 2, 4, 5, 5, 5, 1]
+    },
+    {
+        name: 'Kairi Sane',
+        photo: 'https://assets.imksb.net/sites/13/2017/09/kairisane.png',
+        scores: [4, 2, 5, 4, 1, 2, 4, 3, 2, 4]
+    },
+    {
+        name: 'Aleister Black',
+        photo: 'https://upload.wikimedia.org/wikipedia/commons/0/08/Aleister_Black_2017_WWE.jpg',
+        scores: [3, 4, 3, 3, 5, 4, 4, 1, 3, 2]
     }
 ]
 
--- server.js
+++ server.js
@@ -4,13 +4,12 @@ const bodyParser = require('body-parser');
 const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 3000;
-
 // setting up express to handle data
+app.use(express.static(path.join(__dirname, 'app/public')));
 app.use(bodyParser.urlencoded({
     extended: true
 }));
 app.use(bodyParser.json());
-app.use(express.static('public'));
 
 require('./app/routing/apiRoutes')(app);
 require('./app/routing/htmlRoutes')(app);
